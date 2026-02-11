8e345df3dae89da069b2da2de2a770ca33149f8a
Denys Vuika <denys.vuika@gmail.com>
2026-02-11 14:34:11 +0000

    ai sdk and example

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button';
+import ChatBot from './components/app/chatbot';
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="flex h-screen w-screen">
+      <ChatBot />
     </div>
   );
 }
